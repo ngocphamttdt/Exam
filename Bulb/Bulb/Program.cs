eecec74ba25f0b9b4ac032285f5877b793b7fccc
Pham Thi Thuy Ngoc <ngoc.pham@idealogic.com.vn>
2021-11-28 18:55:14 +0700

    init

---
+++ Bulb/Bulb/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bulb
+{
+    class Program
+    {
+        public static int NumTimesAllBlue(int[] lights)
+        {
+            List<int> moments = new List<int>();
+            int numOfLight = lights.Count();
+            //create array line lights   //var distanceToFirst = turnOnLight - 1;
+            int[] lineLight = new int[numOfLight];
+            foreach (int turnOnLight in lights)
+            {
+                lineLight[turnOnLight - 1] = 1;
+                bool rangeAllTurnOn = true;
+                for (int i = 0; i < turnOnLight; i++)
+                {
+                    if ( lineLight[i] == 0)
+                    {
+                        rangeAllTurnOn = false;
+                        break;
+                    }  
+                }
+                if (rangeAllTurnOn) moments.Add(turnOnLight);
+            }
+            return moments.Count();
+        }
+        static void Main(string[] args)
+        {
+            int[] lights = { 2, 1, 3, 5, 4 }; //[4,1,2,3]
+            int result = NumTimesAllBlue(lights);
+            Console.WriteLine("Hello World!", result);
+        }
+    }
+}
---
+++ Car/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static long carParkingRoof(List<long> cars, int k)
+        {
+            cars.Distinct();
+            cars.Sort();
+            long firstItem = cars.Min();
+            long lastItem = cars.Max();
+
+            long nextItemFromFirst = cars[k - 1];
+            long nextItemFromLast = cars[cars.Count()- k];
+
+            long fromFirstLength = nextItemFromFirst - firstItem + 1;
+            long fromLastLength = lastItem - nextItemFromLast + 1;
+            return fromFirstLength < fromLastLength ? fromFirstLength : fromLastLength;
+
+        }
+        static void Main(string[] args)
+        {
+            var test = carParkingRoof(new List<long> { 2, 6, 7, 12 }, 3);
+            Console.WriteLine("data", test);
+        }
+    }
+}
